07801e0bb9832c186608189515a1de921e8e5460
hamid-dev8 <hamidrezasayah@gmail.com>
2024-12-11 19:39:23 +0330

    WIP/add feature module

---
+++ core/database/src/main/java/com/repoint/database/AppDatabase.kt
@@ -0,0 +1,28 @@
+package com.repoint.database
+
+import androidx.room.Database
+import androidx.room.DatabaseConfiguration
+import androidx.room.InvalidationTracker
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteOpenHelper
+
+@Database(entities = [] , version =  1 , exportSchema = false)
+class AppDatabase : RoomDatabase()
+{
+
+    //abstract  fun dao() : SomeDao
+
+
+    override fun clearAllTables() {
+        TODO("Not yet implemented")
+    }
+
+    override fun createInvalidationTracker(): InvalidationTracker {
+        TODO("Not yet implemented")
+    }
+
+    override fun createOpenHelper(config: DatabaseConfiguration): SupportSQLiteOpenHelper {
+        TODO("Not yet implemented")
+    }
+
+}
